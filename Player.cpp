a1f881eadf2a4c99582f9d13a45ea62b0edcac5d
Oskar Haarklou Veileborg <ohv1020@hotmail.com>
2017-04-22 15:44:41 +0200

    Use camera for player

--- Player.cpp
+++ Player.cpp
@@ -15,7 +15,7 @@ void Player::render(sf::Uint8* pixels, sf::FloatRect camera) {
 	for(int x = -20; x < 20; x++) {
 		for(int y = -20; y < 20; y++) {
 			if(std::hypot(x, y) < 20) {
-				setPixel(pixels, pos.x + x, pos.y + y, sf::Color::Black);
+				setPixel(pixels, camera, {pos.x + x, pos.y + y}, sf::Color::Black);
 			}
 		}
 	}
